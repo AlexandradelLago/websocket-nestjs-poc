0217ae7b086bb9d94cca741e7d6cb0ce959f3445
Davinia Roca <daviniaroca@gmail.com>
2024-10-17 14:53:17 +0200

    hello world and gateway

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+
+  console.log(`🚀 Aplicación iniciada en http://localhost:${port}`);
 }
+
 bootstrap();
---
+++ src/notification/notification.gateway.ts
@@ -0,0 +1,35 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  WebSocketServer,
+  MessageBody,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({ namespace: '/notification', cors: { origin: '*' } })
+export class NotificationGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+
+  afterInit(server: Server) {
+    console.log('WebSocket Gateway initialized');
+  }
+
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): void {
+    console.log(`Received message: ${message}`);
+    this.server.emit('message', message); // Reenvía el mensaje a todos los clientes conectados
+  }
+}
